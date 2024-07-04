1c8f2b90dc475aab6c2381632a304596569bc6dc
RobisonTorres <robison.torres21@gmail.com>
2024-07-04 16:49:46 -0300

    updated lessons.

--- src/JavaFundamentals/CompareObjects.java
+++ src/JavaFundamentals/CompareObjects.java
@@ -1,12 +1,12 @@
 package JavaFundamentals;
 
-public class TestFree {
-
+public class CompareObjects {
 
     public static void main(String[] args) {
-        int maxValue = Integer.MAX_VALUE;
-        int overflow = maxValue + 1;
 
+        /*For objects, the == operator delves deeper, determining if both references
+        point to an identical memory location. It doesn't evaluate content equality.
+        Instead, you'll use the equals() method for that purpose.*/
         String name = new String("Bob");
         String name1 = new String("Bob");
         //String nickname = new String("Rob");
